28714d73a241dba0e224d0fa2821c2a16666edd3
Charles Liu <reallxc@gmail.com>
2022-04-07 11:44:02 +1200

    Added test marking points

--- src/nz/ac/arastudent/xil0393/syntaxtest3/NumberHider.java
+++ src/nz/ac/arastudent/xil0393/syntaxtest3/NumberHider.java
@@ -1,18 +1,23 @@
 package nz.ac.arastudent.xil0393.syntaxtest3;
 
 public class NumberHider {
+    //Test marking point: protected attributes
+    //Test marking point: use of at least one build-in type
     private int targetNumber,count = 0;
 
+    //Test marking point: two constructors
     public  NumberHider(){
+        //Test marking point: seeding of a random number
         this.targetNumber = (int)(Math.random() * 100);
     }
-
     public  NumberHider(int targetNumber){
         this.targetNumber = targetNumber;
     }
 
     public String makeGuess(int guessNumber) {
+        //Test marking point: use of ++
         this.count++;
+        //Test marking point: use of if statement
         if (guessNumber==this.targetNumber) return "You got it in " + count + " trials!";
         else if (guessNumber>this.targetNumber) return "Try lower";
         else return "Try higher";
